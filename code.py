e41b8dcff2694362628d9ebad1a1207774e2f478
surya sangam <114898448+surya-1312@users.noreply.github.com>
2022-11-15 16:11:49 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,14 +1,12 @@
 from tkinter import *
-import tkinter.messagebox
 import speech_recognition as s
 from googletrans import Translator
 
 r=s.Recognizer()
-
-root = tkinter.Tk()
+root = Tk()
 root.title("...Speech__to__Text...")
 root.geometry("960x540")
-img=PhotoImage(file="D:\\downloads\\back.png")                      # give own path of back.png image 
+img=PhotoImage(file="D:\\downloads\\back.png")
 frame=Label(root,image=img)
 frame.place(x=0,y=0)
 
@@ -35,7 +33,7 @@ LANGUAGES = {
 
 #for take input of user 
 def play1():
-    global lab2,lab4
+    #global lab2,lab4
     with s.Microphone() as input:
         root.update()
         audio= r.listen(input)
@@ -43,24 +41,39 @@ def play1():
         Text = r.recognize_google(audio)
         root.update()
         print(Text)
-        
         frame=Frame(root,background="#ffcc00",height=350,width=420)
         frame.place(x=490,y=80)
-        lab3=Label(frame,text="Speak the language for output ",font=("Arial",19),height=1,bg='#ffcc00')
-        lab3.place(x=30,y=10)
-        lab6=Label(frame,text="sentence",bg='#ffcc00',font=("Arial",16))
-        lab6.place(x=20,y=70)
-        lab5=Label(frame,text=Text,bg='#ffcc00',font=("Arial",20))
-        lab5.place(x=20,y=100)
-        lab4=Label(frame,text='',bg='#ffcc00',font=("Arial",16))
-        lab4.place(x=20,y=220)
-        button.config(command=lambda:translate_text(Text))
-        lab2=Label(frame,text="",font=("Arial",20),height=1,bg='#ffcc00')
-        lab2.place(x=20,y=250)
+        lab6=Label(frame,text="Your sentence is :-",bg='#ffcc00',font=("Arial",18))
+        lab6.place(x=20,y=10)
+        b=Text.split()
+        ln=35
+        Text1=""
+        for i in b:
+                j=len(i)
+                if j<ln:
+                        Text1=Text1+i+" "
+                        print(i,end=" ")
+                        ln=ln-j
+                else:
+                        Text1+="\n"
+                        Text1+=i+" "
+                        print("\n",end="")
+                        print(i,end=" ")
+                        ln=35
+               
+        lab5=Label(frame,text=Text1,bg='#ffcc00',font=("Arial",15))
+        lab5.place(x=20,y=50)
+        lab3=Label(frame,text="Speak the language for output:-",font=("Arial",15),height=1,bg='#ffcc00')
+        lab3.place(x=20,y=140)
+        button.config(command=lambda:translate_text(Text,lab2,lab4))
+        lab4=Label(frame,text='',bg='#ffcc00',font=("Arial",15))
+        lab4.place(x=20,y=180)
+        lab2=Label(frame,text="",font=("Arial",15),bg='#ffcc00')
+        lab2.place(x=20,y=220)
 
 #for Translate the language 
-def translate_text(Text):
-        global LANGUAGES,lab2
+def translate_text(Text,lab2,lab4):
+        global LANGUAGES
         lab2['text']=""
         with s.Microphone() as input1:
                 audio1= r.listen(input1)         
@@ -75,13 +88,29 @@ def translate_text(Text):
                 translator= Translator()
                 root.update()
                 lang2= translator.translate(Text,dest=lancode)
-                lab2['text']=lang2.text
+                b=lang2.text
+                b=b.split()
+                ln=35
+                Text1=""
+                for i in b:
+                        j=len(i)
+                        if j<ln:
+                                Text1=Text1+i+" "
+                                print(i,end=" ")
+                                ln=ln-j
+                        else:
+                                Text1+="\n"
+                                Text1+=i+" "
+                                print("\n",end="")
+                                print(i,end=" ")
+                                ln=35
+                lab2['text']=Text1
 
-
-micimg=PhotoImage(file="D:\\downloads\\1232.png")                              # give own path of 1232.png image 
+micimg=PhotoImage(file="D:\\downloads\\1232.png")
 button=Button(root,image=micimg,bg='#ffde59',bd=0,command =lambda:play1())
 button.place(x=190,y=120)
-exitimg=PhotoImage(file="D:\\downloads\\log.png")                              # give own path of log.png image 
+exitimg=PhotoImage(file="D:\\downloads\\log.png")
 button2=Button(root,image=exitimg,bg="#ffcc00",bd=0,command=root.destroy)
 button2.place(x=700,y=425)
+
 root.mainloop()
